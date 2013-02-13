352f5b91d3a839f8d86ab52f42cea4ee02d12e0f
Fedmich <fedmich@gmail.com>
2013-02-13 19:26:32 +0800

    sample_connect

--- database.py
+++ database.py
@@ -20,3 +20,19 @@ def GetSQL_arr(cursor) :
 		rows.append(row)
 
 	return rows
+
+def sample_connect():
+	
+	import MySQLdb
+	conn = MySQLdb.connect (host = "localhost",
+						user = "root",
+						passwd = "",
+						db = "dbname")
+	cursor = conn.cursor ()
+	sql = "SELECT id, field1, field2 FROM table WHERE field1 = 1"
+	cursor.execute (sql)
+
+	rows = GetSQL_arr(cursor)
+
+	cursor.close ()
+	conn.close ()
